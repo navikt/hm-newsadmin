d8f1557ef70c4f64ecefdc707bba87c723648e40
Maria Agnetha Andreassen <maria.agnetha.andreassen@nav.no>
2026-06-18 11:10:30 +0200

    WIP
    Co-authored-by: Tini Tran <tini.tran@nav.no>
    Co-authored-by: Erlend André Høntorp <erlend.andre.hontorp@nav.no>
    Co-authored-by: Andreas Danielsen Fageraas <andreas.danielsen.fageraas@nav.no>

--- client/src/Startside.tsx
+++ client/src/Startside.tsx
@@ -1,5 +1,31 @@
-export const Startside = () => {
+import {Textarea, TextField, VStack} from "@navikt/ds-react";
 
-  return <>Hallo</>
+export const Startside = () => {
+   async function getNews(): Promise<NewsDTO[]> {
+    const res = await fetch("http://localhost:8084/news" , {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    return res.json()
+  }
+
+  type NewsDTO = {
+    id: string
+    title: string
+    body: string
+  }
+
+  console.log(getNews())
+
+  return (
+  <VStack>
+    <TextField label="Tittel"></TextField>
+    <Textarea label="Innhold"></Textarea>
+  </VStack>
+
+  )
 
 }
