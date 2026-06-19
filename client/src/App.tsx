e768d3494f550edc58fe7f55b3d80a26a121dee7
Maria Agnetha Andreassen <maria.agnetha.andreassen@nav.no>
2026-06-19 14:55:21 +0200

    wip: add edit news page
    Co-authored-by: Tini Tran <tini.tran@nav.no>
    Co-authored-by: Erlend André Høntorp <erlend.andre.hontorp@nav.no>
    Co-authored-by: Andreas Danielsen Fageraas <andreas.danielsen.fageraas@nav.no>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,13 @@
-import {Route, Routes} from 'react-router-dom'
-import {Startside} from 'Startside'
-import {CreateNewsPage} from '/actions/CreateNewsPage'
-
+import { Route, Routes } from 'react-router-dom'
+import { Startside } from 'Startside'
+import { CreateNewsPage } from '/actions/CreateNewsPage'
 
 export function App() {
-
   return (
-        <Routes>
-          <Route path="/" element={<Startside />} />
-            <Route path="/createNewsPage" element={<CreateNewsPage />} />
-        </Routes>
+    <Routes>
+      <Route path="/" element={<Startside />} />
+      <Route path="/createNewsPage" element={<CreateNewsPage />} />
+      <Route path="/editNewsPage" element={<CreateNewsPage />} />
+    </Routes>
   )
 }
--- client/src/NewsComponent.tsx
+++ client/src/NewsComponent.tsx
@@ -2,6 +2,7 @@ import { Bleed, BodyLong, Box, Button, HStack, Link, Page, Textarea, TextField, 
 import { ArrowLeftIcon } from '@navikt/aksel-icons'
 import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
+import { PageBlock } from '@navikt/ds-react/Page'
 
 type CreateNewsDto = {
   title: string
@@ -18,35 +19,37 @@ export const NewsComponent = ({ onSubmit }: Props) => {
   return (
     <>
       <Box background="neutral-soft" minHeight={'100vh'}>
-        <VStack gap="space-24" justify={'center'}>
-          <Bleed marginInline={'full space-0'} asChild>
-            <Button variant="tertiary" icon={<ArrowLeftIcon />} onClick={() => navigate('/')}>
-              Tilbake
-            </Button>
-          </Bleed>
-          <Page.Block as="main" width="text">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <VStack gap="space-16">
-                <h2>Opprett Sak</h2>
-                <Box
-                  background="neutral-soft"
-                  borderColor="brand-blue"
-                  padding="space-16"
-                  borderWidth="2"
-                  borderRadius="12 12 0 0"
-                >
-                  <BodyLong align={'center'}>Her skal det være et bilde!</BodyLong>
-                </Box>
-                <TextField {...register('title')} label="Tittel" width="text"></TextField>
-                <Textarea {...register('description')} label="Ingress" maxLength={250}></Textarea>
-                <Textarea {...register('body')} label="Innhold" minRows={10}></Textarea>
-                <Button type="submit" variant={'primary'}>
-                  Opprett sak
-                </Button>
-              </VStack>
-            </form>
-          </Page.Block>
-        </VStack>
+        <HStack justify={'center'}>
+          <h2>Opprett Sak</h2>
+          <VStack gap="space-24" justify={'center'}>
+            <Page.Block as={'header'} gutters>
+              <Button variant="tertiary" icon={<ArrowLeftIcon />} onClick={() => navigate('/')}>
+                Tilbake
+              </Button>
+            </Page.Block>
+            <Page.Block as="main" width="text">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <VStack gap="space-16">
+                  <Box
+                    background="neutral-soft"
+                    borderColor="brand-blue"
+                    padding="space-16"
+                    borderWidth="2"
+                    borderRadius="12 12 0 0"
+                  >
+                    <BodyLong align={'center'}>Her skal det være et bilde!</BodyLong>
+                  </Box>
+                  <TextField {...register('title')} label="Tittel" width="text"></TextField>
+                  <Textarea {...register('description')} label="Ingress" maxLength={250}></Textarea>
+                  <Textarea {...register('body')} label="Innhold" minRows={10}></Textarea>
+                  <Button type="submit" variant={'primary'}>
+                    Opprett sak
+                  </Button>
+                </VStack>
+              </form>
+            </Page.Block>
+          </VStack>
+        </HStack>
       </Box>
     </>
   )
