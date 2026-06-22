eb9526044ec9fe1bab3a7eb543317550e46896ab
Maria Agnetha Andreassen <maria.agnetha.andreassen@nav.no>
2026-06-22 14:37:46 +0200

    style: adjust placement of button in editComponent
    Co-authored-by: Tini Tran <tini.tran@nav.no>
    Co-authored-by: Erlend André Høntorp <erlend.andre.hontorp@nav.no>
    Co-authored-by: Andreas Danielsen Fageraas <andreas.danielsen.fageraas@nav.no>

--- client/src/EditComponent.tsx
+++ client/src/EditComponent.tsx
@@ -54,16 +54,21 @@ export const EditComponent = ({ onSubmit, onDelete, defaultValues }: Props) => {
   return (
     <Box>
       <VStack gap="space-24" justify={'center'}>
-        <Bleed marginInline={'full space-0'} asChild>
-          {/*TODO: fiks styling på knappen?*/}
-          <Button as={Link} href={'/'} variant={'tertiary'} icon={<ArrowLeftIcon />}>
-            Tilbake
-          </Button>
-        </Bleed>
         <Page.Block as="main" width="text">
           <form onSubmit={handleSubmit(onSubmit)}>
             <VStack gap="space-16">
-              <h2>Rediger Sak</h2>
+              <HStack align={'center'} style={{ position: 'relative' }}>
+                <Button
+                  as={Link}
+                  href={'/'}
+                  variant={'tertiary'}
+                  icon={<ArrowLeftIcon />}
+                  style={{ position: 'absolute', right: '100%' }}
+                >
+                  Tilbake
+                </Button>
+                <h2>Rediger Sak</h2>
+              </HStack>
               <Box
                 background="neutral-soft"
                 borderColor="brand-blue"
