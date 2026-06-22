8e9c0b52ce19bbe65f23260595dcee65c7c05a5d
Andreas.Danielsen.Fageraas <andreas.danielsen.fageraas@nav.no>
2026-06-22 15:37:43 +0200

    removed some hopefully useless stuff
    Co-authored-by: Tini Tran <tini.tran@nav.no>
    Co-authored-by: Erlend André Høntorp <erlend.andre.hontorp@nav.no>
    Co-authored-by: Maria Agnetha Andreassen <maria.agnetha.andreassen@nav.no>

--- client/src/mocks/browser.ts
+++
@@ -1,9 +0,0 @@
-import { resetPasswordHandler } from 'mocks/handlers/resetPasswordHandler'
-import { tilGodkjenningHandlers } from 'mocks/handlers/tilGodkjenningHandler'
-import { unleashHandler } from 'mocks/handlers/unleashHandler'
-import { RequestHandler } from 'msw'
-import { setupWorker } from 'msw/browser'
-
-const handlers: RequestHandler[] = [...tilGodkjenningHandlers, ...unleashHandler, ...resetPasswordHandler]
-
-export const worker = setupWorker(...handlers)
--- client/src/mocks/server.ts
+++
@@ -1,4 +0,0 @@
-import { defaultSeriesHandlers } from 'mocks/handlers/defaultSeriesHandlers'
-import { setupServer } from 'msw/node'
-
-export const server = setupServer(...defaultSeriesHandlers)
