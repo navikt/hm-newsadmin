be332ce9871ba308264aad2ebe5d2abc1e29c941
Maria Agnetha Andreassen <maria.agnetha.andreassen@nav.no>
2026-06-22 08:17:07 +0200

    fix: navigation
    Co-authored-by: Tini Tran <tini.tran@nav.no>
    Co-authored-by: Erlend André Høntorp <erlend.andre.hontorp@nav.no>
    Co-authored-by: Andreas Danielsen Fageraas <andreas.danielsen.fageraas@nav.no>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import { Startside } from 'Startside'
 import { CreateNewsPage } from '/actions/CreateNewsPage'
+import { EditNewsPage } from 'actions/EditNewsPage.tsx'
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Startside />} />
       <Route path="/createNewsPage" element={<CreateNewsPage />} />
-      <Route path="/editNewsPage" element={<CreateNewsPage />} />
+      <Route path="/editNewsPage" element={<EditNewsPage />} />
     </Routes>
   )
 }
