87ae6a5f0f60ce5f6450e50b0ce37d8b3173d73e
Andreas.Danielsen.Fageraas <andreas.danielsen.fageraas@nav.no>
2026-06-23 09:26:33 +0200

    actually removed filler files
    Co-authored-by: Tini Tran <tini.tran@nav.no>
    Co-authored-by: Erlend André Høntorp <erlend.andre.hontorp@nav.no>
    Co-authored-by: Maria Agnetha Andreassen <maria.agnetha.andreassen@nav.no>

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,6 @@ import {createRoot} from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 
 import {initInstrumentation} from 'faro/faro'
-import {initMsw} from 'mocks/initMsw'
-import {initSkyra} from 'utils/skyra'
 import {initUmami} from 'utils/umami'
 
 import '@navikt/ds-css'
@@ -12,24 +10,20 @@ import {App} from 'App'
 
 export function baseUrl(url: string = '') {
     if (process.env.NODE_ENV === 'production') {
-        return `/adminregister${url}`
+        return `/newsadmin${url}`
     } else {
         return url
     }
 }
 
-initMsw().then(() => {
-    initUmami()
-    initInstrumentation()
-    initUmami()
-    initSkyra()
+initUmami()
+initInstrumentation()
 
-    const container = document.getElementById('root')!
-    createRoot(container).render(
-        <>
-            <BrowserRouter basename={baseUrl()}>
-                <App />
-            </BrowserRouter>
-        </>
-    )
-})
+const container = document.getElementById('root')!
+createRoot(container).render(
+    <>
+        <BrowserRouter basename={baseUrl()}>
+            <App />
+        </BrowserRouter>
+    </>
+)
