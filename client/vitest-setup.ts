96624bb746bb26a211b4e64066167174fb9f6521
Atle Nordland <atle.nordland@nav.no>
2026-07-03 13:22:16 +0200

    rydding: fjern unleash-referanser

--- client/vitest-setup.ts
+++ client/vitest-setup.ts
@@ -1,10 +1,8 @@
-import { toHaveNoViolations } from 'jest-axe'
 import { expect, vi } from 'vitest'
 
 import * as matchers from '@testing-library/jest-dom/matchers'
 
 expect.extend(matchers)
-expect.extend(toHaveNoViolations)
 
 vi.mock('environments', () => ({
   HM_REGISTER_URL: vi.fn(() => 'http://localhost:8080'),
