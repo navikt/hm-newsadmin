011f6887f4c8e609a590a6995ce59b15669c202c
Maria Agnetha Andreassen <maria.agnetha.andreassen@nav.no>
2026-07-06 09:30:42 +0200

    fix: fix create news button from being able to be spammed
    Co-authored-by: Tini Tran <tini.tran@nav.no>
    Co-authored-by: Erlend André Høntorp <erlend.andre.hontorp@nav.no>
    Co-authored-by: Andreas Danielsen Fageraas <andreas.danielsen.fageraas@nav.no>

--- client/src/actions/EditNewsPage.tsx
+++ client/src/actions/EditNewsPage.tsx
@@ -3,27 +3,37 @@ import useSWR, { useSWRConfig } from 'swr'
 import { deleteNews } from 'utils/api-util.ts'
 import { NewsAdmin } from 'pages/NewsAdmin.tsx'
 import { NewsFormValues } from 'komponenter/useNewsForm.ts'
+import { useState } from 'react'
 
 export const EditNewsPage = () => {
   const navigate = useNavigate()
   const { id } = useParams()
   const { mutate } = useSWRConfig()
   const { data: news } = useSWR(`/news/${id}`, () => fetch(`/news/${id}`).then((res) => res.json()))
+  const [loading, setLoading] = useState(false)
 
   async function editNews(data: NewsFormValues) {
-    const res = await fetch(`/admin/news/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    })
-    if (res.ok) {
-      await mutate('news')
-      await mutate(`/news/${id}`)
-      navigate('/')
+    setLoading(true)
+    try {
+      const res = await fetch(`/admin/news/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+      if (res.ok) {
+        await mutate('news')
+        await mutate(`/news/${id}`)
+        navigate('/')
+      }
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setLoading(false)
     }
   }
+
   if (!news) return <div></div>
 
   async function handleDelete() {
@@ -31,5 +41,5 @@ export const EditNewsPage = () => {
     navigate('/')
   }
 
-  return <NewsAdmin onSubmit={editNews} onDelete={handleDelete} defaultValues={news} newsId={id} />
+  return <NewsAdmin loading={loading} onSubmit={editNews} onDelete={handleDelete} defaultValues={news} newsId={id} />
 }
