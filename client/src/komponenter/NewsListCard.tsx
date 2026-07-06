c30ee4c89050c0816a9de8a0777c0dffb4a5cf2d
Maria Agnetha Andreassen <maria.agnetha.andreassen@nav.no>
2026-07-06 08:44:16 +0200

    style: improve responsive design
    Co-authored-by: Tini Tran <tini.tran@nav.no>
    Co-authored-by: Erlend André Høntorp <erlend.andre.hontorp@nav.no>
    Co-authored-by: Andreas Danielsen Fageraas <andreas.danielsen.fageraas@nav.no>

--- client/src/komponenter/NewsListCard.tsx
+++ client/src/komponenter/NewsListCard.tsx
@@ -3,27 +3,14 @@ import { NewsDTO } from 'utils/admin-util.ts'
 import { useNavigate } from 'react-router-dom'
 import NewsImage from 'komponenter/NewsImage.tsx'
 import { statusTagProps } from 'utils/news-filter-util.ts'
+import './NewsListCard.scss'
 
 export default function NewsListCard({ news }: { news: NewsDTO }) {
   const navigate = useNavigate()
   const { label, variant } = statusTagProps[news.status]
   return (
-    <LinkCard
-      onClick={() => navigate(`/news/${news.id}/edit`)}
-      style={{ height: '14rem', paddingInlineStart: '26rem' }}
-    >
-      <Box
-        style={{
-          position: 'absolute',
-          overflow: 'hidden',
-          borderTopLeftRadius: 'calc(var(--ax-radius-12) - 1px',
-          borderBottomLeftRadius: 'calc(var(--ax-radius-12) - 1px)',
-          left: 0,
-          top: 0,
-          bottom: 0,
-          width: '400px',
-        }}
-      >
+    <LinkCard onClick={() => navigate(`/news/${news.id}/edit`)} className={'card'}>
+      <Box className={'image'}>
         <NewsImage imageUrl={news.image_url} />
       </Box>
       <LinkCard.Title
