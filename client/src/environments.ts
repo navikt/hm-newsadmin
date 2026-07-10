7a54d609d0a0fd88f3fdc1c6fb26d70cc81d79da
Tini.Tran <tini.tran@nav.no>
2026-07-10 11:14:02 +0200

    fix: fikse bilde slik at den ikke bruker external url
    Co-authored-by: Erlend André Høntorp <erlend.andre.hontorp@nav.no>
    Co-authored-by: Maria Agnetha Andreassen <maria.agnetha.andreassen@nav.no>
    Co-authored-by: Andreas Danielsen Fageraas <andreas.danielsen.fageraas@nav.no>

--- client/src/environments.ts
+++ client/src/environments.ts
@@ -1,18 +1,4 @@
-export const HM_REGISTER_URL = () => {
-  if (window.location.hostname.includes('ansatt')) {
-    return window.appSettings.VITE_HM_REGISTER_EXTERNAL_URL
-  } else {
-    return window.appSettings.VITE_HM_REGISTER_URL ?? 'http://localhost:3000'
-  }
-}
-
 export const FINNHJELPEMIDDEL_PUBLIC_URL = () =>
   window.appSettings.VITE_HM_REGISTER_URL ?? 'http://localhost:3000'
 
-export const IMAGE_PROXY_URL = () => {
-  if (window.location.hostname.includes('ansatt')) {
-    return window.appSettings.VITE_IMAGE_PROXY_EXTERNAL_URL
-  } else {
-    return window.appSettings.VITE_IMAGE_PROXY_URL
-  }
-}
+export const IMAGE_PROXY_URL = () => window.appSettings.VITE_IMAGE_PROXY_URL
--- client/src/window.d.ts
+++ client/src/window.d.ts
@@ -8,8 +8,6 @@ declare global {
       MILJO?: 'local' | 'dev-gcp' | 'prod-gcp' | string
       VITE_HM_REGISTER_URL?: string
       VITE_IMAGE_PROXY_URL?: string
-      VITE_HM_REGISTER_EXTERNAL_URL?: string
-      VITE_IMAGE_PROXY_EXTERNAL_URL?: string
       VITE_FARO_URL?: string
     }
   }
