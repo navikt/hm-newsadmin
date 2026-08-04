4e6c5445e405108df43526d4ca240aad6a59c920
marte pedersen <marte.pedersen@nav.no>
2026-08-04 13:19:37 +0200

    fiksa dato-logikk for visning av riktig tags på nyhets-korta

--- client/src/utils/news-filter-util.ts
+++ client/src/utils/news-filter-util.ts
@@ -15,6 +15,7 @@ export type DisplayStatus = 'aktiv' | 'planlagt' | 'utløpt' | 'utkast'
 export function getDisplayStatus(news: NewsDTO): DisplayStatus {
   if (news.status === 'DRAFT') return 'utkast'
   const now = new Date()
+  now.setHours(0, 0, 0, 0) // for at sammenligninga blir riktig, datoene er ved midnatt 0:0:0:0
   if (new Date(news.publishedFrom) > now) return 'planlagt'
   if (new Date(news.publishedTo) < now) return 'utløpt'
   return 'aktiv'
